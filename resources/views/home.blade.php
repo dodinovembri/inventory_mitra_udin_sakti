5cee5a7c16d33728cd419c097d46ec129c62427c
dodinovembri <dodinovembri32@gmail.com>
2021-09-01 10:13:59 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -181,7 +181,7 @@
 											<div class="modal-dialog" role="document">
 												<div class="modal-content">
 													<div class="modal-header">
-														<h6 class="modal-title" id="exampleModalLabel">Barang Keluar</h6>
+														<h6 class="modal-title" id="exampleModalLabel">Hapus Data</h6>
 														<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 															<span aria-hidden="true"><i data-feather="x"></i></span>
 														</button>
